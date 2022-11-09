bde86c145c2053fb77b53ceb3cf81c890a6e546a
ronglecat <ronglecat@163.com>
2022-11-09 22:20:24 +0800

    docs: changelog

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 # node_modules/.bin/webpack --mode production
+# 下面的目标路径是 Alfred Workflow 的目录
+export targetDir="/Users/caohaoxia/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows/user.workflow.37EAE88B-2588-4C52-95D8-272748DE8E53"
 rm -rf $PWD/dist
 export NODE_ENV=production
 rollup -c
@@ -7,5 +9,5 @@ rollup -c
 cp -rf $PWD/runtime $PWD/dist/
 cp -rf $PWD/assets $PWD/dist/
 
-# 下面复制的目标路径是 Alfred Workflow 的目录
-cp -rf $PWD/dist/* "/Users/caohaoxia/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows/user.workflow.37EAE88B-2588-4C52-95D8-272748DE8E53"
+rm -rf $targetDir/*.js
+cp -rf $PWD/dist/* $targetDir
